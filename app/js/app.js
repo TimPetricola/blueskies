8bebce3f95e0379bde1d873ae4e9e253ac69fbdc
Tim Petricola <tim.petricola@gmail.com>
2015-10-28 11:25:07 +0100

    Minor JS refactoring

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,15 @@
 const util = (() => {
-  const nodeArray = (nodeList) => [].slice.call(nodeList);
+  const dateToHumanTime = (date) => {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const period = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${hours}:${minutes}${period}`;
+  };
 
-  return { nodeArray };
+  return { dateToHumanTime };
 })();
 
 const Form = ((util) => {
@@ -13,7 +21,7 @@ const Form = ((util) => {
 
   const selectedInterests = () => {
     const selected = document.querySelectorAll('.js-interest-input:checked');
-    return util.nodeArray(selected).map((s) => s.value);
+    return [...selected].map((s) => s.value);
   };
 
   const updateSampleLink = () => {
@@ -27,25 +35,15 @@ const Form = ((util) => {
     sampleLink.href = href;
   };
 
-  const dateToHumanTime = (date) => {
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    const period = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-    return `${hours}:${minutes}${period}`;
-  };
-
-  util.nodeArray(tzInputs).forEach((input) => {
+  [...tzInputs].forEach((input) => {
     input.value = new Date().getTimezoneOffset() / 60;
   });
 
-  util.nodeArray(localTimeLabel).forEach((node) => {
-    node.innerText = dateToHumanTime(new Date());
+  [...localTimeLabel].forEach((node) => {
+    node.innerText = util.dateToHumanTime(new Date());
   });
 
-  util.nodeArray(interestsInputs).forEach((input) => {
+  [...interestsInputs].forEach((input) => {
     input.addEventListener('change', updateSampleLink);
   });
 
