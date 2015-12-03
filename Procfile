4e189240f7553006af082792d2fd92f4eeabd374
Tim Petricola <tim.petricola@gmail.com>
2015-12-03 18:01:33 +0100

    🌅Good bye Gulp, Hello Webpack

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 web: bundle exec puma -C ./lib/puma.rb
 worker: bundle exec sidekiq -r ./app.rb -q digest,2 -q default -c 7
 console: bundle exec pry -r ./app
-dev: ./node_modules/.bin/gulp watch
+dev: webpack --config webpack.config.js --watch
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var Clean = require('clean-webpack-plugin')
+var fs = require('fs')
+
+var ASSETS_MANIFEST = './assets-manifest.json'
+var ENTRIES = {
+  bundle: [
+    './app/js/app.js',
+    './app/css/app.css'
+  ],
+  email: './app/css/email.css'
+}
+var ASSETS_OUTPUT_PATH = './public/assets'
+
+module.exports = {
+  devtool: 'eval',
+  entry: ENTRIES,
+  output: {
+    path: ASSETS_OUTPUT_PATH,
+    filename: '[name]-[chunkhash].js'
+  },
+  minimize: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        include: __dirname,
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css!cssnext')
+      }
+    ]
+  },
+  plugins: [
+    new Clean(ASSETS_OUTPUT_PATH),
+    new ExtractTextPlugin('[name]-[contenthash].css'),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+    new webpack.NoErrorsPlugin(),
+    function () {
+      this.plugin('done', function(stats) {
+        var chunks = stats.toJson().assetsByChunkName
+        var assets = {}
+        Object.keys(chunks).forEach(function (name) {
+          chunks[name].forEach(function (chunk) {
+            assets[name + '.' + chunk.split('.').pop()] = chunk
+          })
+        })
+        fs.writeFileSync(ASSETS_MANIFEST, JSON.stringify(assets, null, 2))
+      });
+    }
+  ]
+}
