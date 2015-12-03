7be4da96018ad0f2dc6dd60277d2332b5d9cc15e
Tim Petricola <tim.petricola@gmail.com>
2015-12-03 17:08:53 +0100

    JS style

--- app/js/app.js
+++ app/js/app.js
@@ -1,82 +1,75 @@
-const util = (() => {
-  const dateToHumanTime = (date) => {
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    const period = hours >= 12 ? 'PM' : 'AM';
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-    return `${hours}:${minutes}${period}`;
-  };
+const dateToHuman = (date) => {
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
+  const period = hours >= 12 ? 'PM' : 'AM'
+  hours = hours % 12
+  hours = hours === 0 ? 12 : hours
+  minutes = minutes < 10 ? `0${minutes}` : minutes
+  return `${hours}:${minutes}${period}`
+}
 
-  return { dateToHumanTime };
-})();
+// Form
+const tzInputs = document.querySelectorAll('.js-input-timezone')
+const localTimeLabel = document.querySelectorAll('.js-local-time-human')
+const interestsInputs = document.querySelectorAll('.js-interest-input')
+const sampleLink = document.querySelector('.js-sample-link')
+const samplePath = sampleLink.href
 
-const Form = ((util) => {
-  const tzInputs = document.querySelectorAll('.js-input-timezone');
-  const localTimeLabel = document.querySelectorAll('.js-local-time-human');
-  const interestsInputs = document.querySelectorAll('.js-interest-input');
-  const sampleLink = document.querySelector('.js-sample-link');
-  const samplePath = sampleLink.href;
+const selectedInterests = () => {
+  const selected = document.querySelectorAll('.js-interest-input:checked')
+  return [...selected].map((s) => s.value)
+}
 
-  const selectedInterests = () => {
-    const selected = document.querySelectorAll('.js-interest-input:checked');
-    return [...selected].map((s) => s.value);
-  };
+const updateSampleLink = () => {
+  let href = samplePath
+  const selected = selectedInterests()
 
-  const updateSampleLink = () => {
-    let href = samplePath;
-    const selected = selectedInterests();
+  if (selected.length) {
+    href += '?' + selected.map((id) => 'interests[]=' + id).join('&')
+  }
 
-    if(selected.length) {
-      href += '?' + selected.map((id) => 'interests[]=' + id).join('&');
-    }
+  sampleLink.href = href
+}
 
-    sampleLink.href = href;
-  };
+;[...tzInputs].forEach((input) => {
+  input.value = new Date().getTimezoneOffset() / 60
+})
 
-  [...tzInputs].forEach((input) => {
-    input.value = new Date().getTimezoneOffset() / 60;
-  });
+;[...localTimeLabel].forEach((node) => {
+  node.innerText = dateToHuman(new Date())
+})
 
-  [...localTimeLabel].forEach((node) => {
-    node.innerText = util.dateToHumanTime(new Date());
-  });
+;[...interestsInputs].forEach((input) => {
+  input.addEventListener('change', updateSampleLink)
+})
 
-  [...interestsInputs].forEach((input) => {
-    input.addEventListener('change', updateSampleLink);
-  });
+updateSampleLink()
 
-  updateSampleLink();
-})(util);
+// Interests spinner
+const spinner = document.querySelector('.js-interest-spinner')
 
-const InterestsSpinner = (() => {
-  const spinner = document.querySelector('.js-interest-spinner');
+const current = document.querySelector('.js-interest-spinner-current')
+const next = document.querySelector('.js-interest-spinner-next')
+const words = spinner.getAttribute('data-words').split(',')
 
-  const current = document.querySelector('.js-interest-spinner-current');
-  const next = document.querySelector('.js-interest-spinner-next');
-  const words = spinner.getAttribute('data-words').split(',');
+let index = 0
+let nextIndex = 1
 
-  let index = 0;
-  let nextIndex = 1;
+next.textContent = words[nextIndex]
+spinner.style.width = `${current.offsetWidth}px`
 
-  next.textContent = words[nextIndex];
-  spinner.style.width = `${current.offsetWidth}px`;
+const spin = (first) => {
+  index = nextIndex
+  nextIndex++
 
-  const spin = (first) => {
-    index = nextIndex;
-    nextIndex++;
+  if (nextIndex >= words.length) {
+    nextIndex = 0
+  }
 
-    if(nextIndex >= words.length) {
-      nextIndex = 0;
-    }
+  spinner.style.width = `${next.offsetWidth}px`
 
-    spinner.style.width = `${next.offsetWidth}px`;
+  current.textContent = words[index]
+  next.textContent = words[nextIndex]
+}
 
-    current.textContent = words[index];
-    next.textContent = words[nextIndex];
-  };
-
-  setInterval(spin, 3000);
-})();
-
+setInterval(spin, 3000)
