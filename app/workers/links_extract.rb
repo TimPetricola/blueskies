48ac92d0ef9fb877ac4babcadecf8c55f869b272
Tim Petricola <tim.petricola@gmail.com>
2016-03-20 14:27:05 +0100

    No need to extract links that often

--- app/workers/links_extract.rb
+++ app/workers/links_extract.rb
@@ -4,7 +4,7 @@ module BlueSkies
       include Sidekiq::Worker
       include Sidetiq::Schedulable
 
-      recurrence { hourly.minute_of_hour(0, 15, 30, 45) }
+      recurrence { hourly }
 
       def perform
         BlueSkies::Models::Link.unextracted.each do |link|
